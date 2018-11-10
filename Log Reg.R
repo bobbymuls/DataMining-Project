c59957d71a937749e322c88eb04b64e56432378c
yikailim93 <43842036+yikailim93@users.noreply.github.com>
2018-11-10 18:04:05 +0800

    Add files via upload

--- Log Reg.R
+++ Log Reg.R
@@ -5,33 +5,40 @@ library(ROCR)
 library(lmtest)
 
 ks <- read_excel("kslatest.xlsx")
+# redundant variables (columns) are manually deleted from the excel sheet itself
+# kslatest.xlsx contains only variables main_category, country, launched_month, backers,
+# usd_goal_real, state, duration and textlength
+
 ks$state <- as.factor(ks$state)
 ks$launched_month <- as.factor(ks$launched_month)
+ks$usd_goal_real <- scale(ks$usd_goal_real)
+ks$duration <- scale(ks$duration)
+ks$textlength <- scale(ks$textlength)
 
 View(ks)
 attach(ks)
 
-#Creation of training (70%) and test (30%) set
+# Creation of training (70%) and test (30%) set
 set.seed(4510)
 train <- sample(nrow(ks), 0.70 * nrow(ks))
 ks.train <- ks[train, ]
 ks.test <- ks[-train, ]
 #-----------------------------------------------------------------------------------------------
-### A full logistic regression model was built with State as the response variable against the rest of the 
-### 7 predictor variables - Main Category, Country, Launched Month, Duration, Backers, Goal and Textlength. 
-### Step-wise variable selection method was used to identify the most important variables in the logistic regression model.
+# A full logistic regression model was built with State as the response variable against the rest of the 
+# 7 predictor variables - Main Category, Country, Launched Month, Duration, Backers, Goal and Textlength. 
+# Step-wise variable selection method was used to identify the most important variables in the logistic regression model.
 
 nullmodel <- glm(state~1, data = ks.train, family = "binomial")
 fullmodel <- glm(state~., data = ks.train, family = "binomial")
 
-#Backward step-wise selection
+# Backward step-wise selection
 backward.model <- step(fullmodel, direction = "backward")
 
-#Forward step-wise selection
+# Forward step-wise selection
 forward.model <- step(nullmodel, scope = list(lower = nullmodel, upper = fullmodel), direction = 'forward')
 
-### Both backward and forward step-wise selection methods give us the same final model.
-### The logistic regression model is chonsen with the lowest AIC of 18871.39 as criterion.
+# Both backward and forward step-wise selection methods give us the same final model.
+# The logistic regression model is chonsen with the lowest AIC of 18871.39 as criterion.
 
 ##### Likelihood ratio test
 ##### mod.fit.one <- glm(state ~., data=ks.train, family="binomial")
@@ -39,11 +46,11 @@ forward.model <- step(nullmodel, scope = list(lower = nullmodel, upper = fullmod
 ##### anova(mod.fit.one, mod.fit.two, test ="Chisq")
 ##### lrtest(mod.fit.one, mod.fit.two)
 
-#The final model from backward and forward step-wise selection is as below
+# The final model from backward and forward step-wise selection is as below
 fit.glm <- glm(state ~ main_category + country + backers + usd_goal_real + duration + textlength, data = ks.train, family = "binomial")
 summary(fit.glm)
 #-----------------------------------------------------------------------------------------------
-#10-fold CV
+# 10-fold CV
 ctrl <- trainControl(method = "cv", number = 10)
 
 fit.model <- train(state ~ main_category + country + backers + usd_goal_real + duration + textlength, data = ks.train, 
@@ -53,14 +60,14 @@ fit.model <- train(state ~ main_category + country + backers + usd_goal_real + d
 
 pred <- predict(fit.model, newdata = ks.test)
 confusionMatrix(data = pred, ks.test$state)
-### Accuracy : 0.8974
+# Accuracy : 0.8974
 
-### Classification Rate
+# Classification Rate
 accuracy <- table(pred, t(ks.test[,"state"]))
 (1-sum(diag(accuracy))/sum(accuracy))*100 # Calculate misclassification rate
-### 10.25641%
+# 10.25641%
 #-----------------------------------------------------------------------------------------------
-#Parameters tuning & 10-fold CV
+# Parameters tuning & 10-fold CV
 grid.glmnet <- expand.grid(alpha = c(0,  .1,  .2, .4, .6, .8, 1), lambda = seq(.01, .2, length = 20))
 ctrl.glmnet <- trainControl(method = "cv", number = 10)
 fit.glmnet <- train(state ~ main_category + country + backers + usd_goal_real + duration + textlength, data = ks.train,
@@ -70,7 +77,7 @@ fit.glmnet <- train(state ~ main_category + country + backers + usd_goal_real + 
                     trControl = ctrl.glmnet)
 
 fit.glmnet
-### 1.0    0.01    0.7920911  0.5535618915
+### 1.0    0.01    0.7921558  5.538964e-01
 ### Accuracy was used to select the optimal model using the largest value.
 ### The final values used for the model were alpha = 1 and lambda = 0.01.
 
@@ -84,11 +91,3 @@ accuracy.glmnet <- table(pred.glmnet, t(ks.test[,"state"]))
 (1-sum(diag(accuracy.glmnet))/sum(accuracy.glmnet))*100 # Calculate misclassification rate
 # 21.57992%
 #-----------------------------------------------------------------------------------------------
-# ROC Curve & AUC
-pr <- predict(pred.glmnet, ks.test$state)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
-
-auc <- performance(pr, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
