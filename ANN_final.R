d89a9d52e0f1b6339604a20489b839dd15c99fce
foojikai <43903743+foojikai@users.noreply.github.com>
2018-11-19 09:00:15 +0800

    Update and rename ANN_after onehot.R to ANN_final.R

--- ANN_final.R
+++ ANN_final.R
@@ -20,7 +20,8 @@ drops = c("X",
           "currency",
           "usd.pledged",
           "usd_goal_real",
-          "category"
+          "category",
+          "goal"
 )
 
 #One hot encoding the training set
@@ -36,6 +37,7 @@ colnames(training_set)[8] = 'main_categoryFilmVideo'
 training_set$state = as.numeric(as.character(factor(training_set$state,
                                                     levels = c('failed', 'successful'),
                                                     labels = c(0, 1))))
+#Check the strings of training set
 #str(training_set)
 
 #One hot encoding the test set
@@ -51,8 +53,10 @@ colnames(test_set)[8] = 'main_categoryFilmVideo'
 test_set$state = as.numeric(as.character(factor(test_set$state,
                                                 levels = c('failed', 'successful'),
                                                 labels = c(0, 1))))
+#Check the strings of test set
 #str(test_set)
 
+##Standardizing of dataset
 #Computing mean and sd of training set
 train_mean = apply(training_set[-1], 2, mean)
 train_sd = apply(training_set[-1], 2, sd)
@@ -66,63 +70,83 @@ training_set[-1] = scale(training_set[-1],
 test_set[-1] = scale(test_set[-1],
                      center = train_mean,
                      scale = train_sd)
+##
 
 #Writing as a formula to insert into the neuralnet package
 n = names(training_set)
 f = as.formula(paste("state ~", paste(n[!n %in% "state"], collapse = " + ")))
 
-#5 fold cross validation
+##5 fold cross validation
 K = 5
 accuracy = NULL
 costNNCV = NULL
 neuronMSE = NULL
 neuronaccuracy = NULL
 #Fitting Neural Network to select parameters for the neural network
-neuronsize = c(25,30,35) #or any other vector with possible neuron sizes
+#WARNING: Will take very long to run the full length of neuronsize as the dataset is too huge
+#neuron size will take reference from [min,max] =  [29, 42] 
+#where starting point is 2/3*predictor variables + output variables, max is number of predictor variables (from report)
+neuronsize = c(29:42)
+#neuronsize = 40 #the optimal neuron count with lowest MSE, skip running all instances of c(29,42)
 len = length(neuronsize)
 for (i in 1:len) {
   for(k in 1:K){
-    index = sample(seq_len(nrow(training_set)),round(0.7*nrow(training_set)))
     #Pseudo-training and pseudo-test set (70-30 split)
+    index = sample(seq_len(nrow(training_set)),round(0.7*nrow(training_set)))
     trainNNCV = training_set[index,]
     testNNCV = training_set[-index,]
     
     NNCV <- neuralnet(f,data = trainNNCV,hidden = neuronsize[i],
-                      learningrate.factor=list(minus=c(0.5), plus=c(1.2)), stepmax = 1e6,linear.output = F,lifesign = 'full')
+                      learningrate.factor=list(minus=c(0.5), plus=c(1.2)), stepmax = 1e7,linear.output = F,lifesign = 'full')    
     
     #Prediction
     mypredictNNCV = compute(NNCV,testNNCV[,-1])$net.result
     
-    #MSE (cross validation error)
+    #MSE (cross-validation error)
     costNNCV[k] = sum((testNNCV$state - mypredictNNCV)^2)/nrow(testNNCV)
     
-    #Accuracy of result
+    ##Accuracy of result (optional)
     mypredictroundedNNCV = sapply(mypredictNNCV,round,digits=0) #to round off as State is either 0 or 1
-    #To compute confusion matrix
+    #To compute confusion matrix 
     confmat = table(mypredictroundedNNCV, testNNCV$state)
     misclassificationrate = (1-sum(diag(confmat))/sum(confmat))*100
     accuracy[k] = 100-misclassificationrate
+    ##
   }
-  #Mean cost & accuracy
-  neuronaccuracy[i] =  mean(accuracy)
+  #Mean cost & accuracy 
+  neuronaccuracy[i] =  mean(accuracy) #optional
   neuronMSE[i] = mean(costNNCV)
 }
-#Return the vectors of each neuron corresponding MSE/accuracy
+#Return the vectors of each neuron corresponding MSE/accuracy (optional)
 #neuronaccuracy
 #neuronMSE
 #To select the neuron size that returns the lowest MSE
 (optimalneuron = neuronsize[match(min(neuronMSE),neuronMSE)])
+#optimalneuron is 40, with MSE = 0.12216554166,  accuracy = 87.68518519%.
+##
 
 #Running the neuralnet with tuned parameters
 NN <- neuralnet(f,data = training_set,hidden = optimalneuron,learningrate.factor=list(minus=c(0.5), plus=c(1.2)),stepmax = 1e6,linear.output = F,lifesign = 'full')
+#Training time:
+#842000	min thresh: 0.01010434936
+#842972	error: 247.39991	time: 1.43 days
 
 #Prediction
 mypredictNN = compute(NN,test_set[,-1])$net.result
 
-#Accuracy of result
+##Accuracy of result
 mypredictroundedNN = sapply(mypredictNN,round,digits=0) #to round off as State is either 0 or 1
 #To compute confusion matrix
 confmat = table(mypredictroundedNN, test_set$state)
 misclassificationrate = (1-sum(diag(confmat))/sum(confmat))*100
-(accuracy = 100-misclassificationrate) #Returns accuracy of model on predicting test set
+(accuracy = 100-misclassificationrate)
+##
 
+#confmat for 40 neurons
+#mypredictroundedNN
+#     0    1
+#0 6726  760
+#1  761 4779
+
+#ACcuracy for 40 neurons
+#88.32335329%
